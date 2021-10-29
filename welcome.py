09d9b71a57cfb0e54f4bb886d1f4099444374902
aakash <aakashvishwakarma72@gmail.com>
2021-10-29 16:36:12 +0530

    changed welcome.py

--- welcome.py
+++ welcome.py
@@ -7,12 +7,12 @@ from base64 import b64decode
 kms_client=boto3.client('kms')
 encrypted_snow_creds = os.environ['SNOW_CREDS']
 
-snow_creds = kms_client.decrypt(
+cred = kms_client.decrypt(
     CiphertextBlob=b64decode(encrypted_snow_creds),
     EncryptionContext={'LambdaFunctionName': os.environ['AWS_LAMBDA_FUNCTION_NAME']}
 )['Plaintext'].decode('utf-8')
 
-cred = json.loads(snow_creds)
+cred = json.loads(cred)
 client_id = cred['CLIENT_ID']
 client_secret = cred['CLIENT_SECRET']
 user_name = cred['USERNAME']
