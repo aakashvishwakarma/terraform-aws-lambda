7a2e978a82a5cadc9c3ebaf5117faea5598f0b07
aakashvishwakarma <50501190+aakashvishwakarma@users.noreply.github.com>
2022-01-12 21:24:29 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -5,21 +5,19 @@ import os
 from base64 import b64decode
 
 kms_client=boto3.client('kms')
-encrypted_snow_creds = os.environ['SNOW_CREDS']
+encrypted_snow_creds = os.environ['cred']
 
-cred = kms_client.decrypt(
+creds = kms_client.decrypt(
     CiphertextBlob=b64decode(encrypted_snow_creds),
     EncryptionContext={'LambdaFunctionName': os.environ['AWS_LAMBDA_FUNCTION_NAME']}
 )['Plaintext'].decode('utf-8')
-
-cred = json.loads(cred)
-client_id = cred['CLIENT_ID']
-client_secret = cred['CLIENT_SECRET']
-user_name = cred['USERNAME']
-password = cred['PASSWORD']
-
-
-def lambda_handler(event, context):
+print(creds)
+cred = json.loads(creds)
+client_id = cred['client_id']
+print(client_id)
+password = cred['password']
+print(password)
+def handler(event, context):
     print("hello from lambda")
     return{
         'Statuscode': '200'
